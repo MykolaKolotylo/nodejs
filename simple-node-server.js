23b8ec5021dc11f11926c8471f288bccd2f0651b
Nick <mykolakolotylo@gmail.com>
2022-07-31 23:15:18 +0300

    [simple node server]: routing request

--- simple-node-server.js
+++ simple-node-server.js
@@ -1,11 +1,27 @@
 const http = require('http');
+const fs = require("fs");
 const DEFAULT_PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(`url: ${req.url}; method: ${req.method}; headers :${req.headers}; `);
-
+    const url = req.url;
+    const method = req.method;
     // to read more about headers visit https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
     res.setHeader('Content-Type', 'text/html');
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.write('<html lang="us">');
     res.write('<body><title>My First Page</title></body>');
     res.write('<body><h1>Hello from the server</h1></body>');
