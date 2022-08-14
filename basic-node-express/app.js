2069d46324cb3769067566a3711f584717dfdc5d
Nick <mykolakolotilo@gmail.com>
2022-08-14 19:25:19 +0300

    simple express

--- basic-node-express/app.js
+++ basic-node-express/app.js
@@ -1,7 +1,14 @@
-const http = require('http');
 const express = require('express');
-
 const app = express();
-const server = http.createServer(app);
 
-server.listen(3000);
+app.use('/about', (req, res, next) => {
+    console.log('in the about');
+    res.send('<h1>Hello from About</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('in the another middleware v3!');
+    res.send('<h1>Hello from Next Express!</h1>');
+});
+
+app.listen(3000);
