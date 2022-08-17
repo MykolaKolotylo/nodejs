a86acbb645d6d0c22c08c9812342fee45ae04070
Nick <mykolakolotilo@gmail.com>
2022-08-17 09:24:31 +0300

    [basic-node-express]: filtering by main route

--- basic-node-express/routes/admin.js
+++ basic-node-express/routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     console.log('add-product');
-    res.send('<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="product"><button type="submit">Add Product</button></form>');
 });
 
 router.post('/product', (req, res, next) => {
